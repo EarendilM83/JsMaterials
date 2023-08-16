57c91730f8004fd304a64518ebf2372cbb5ad803
EarendilM83 <nukri.tusishvili@gmail.com>
2023-08-16 23:58:55 +0400

    js examples

---
+++ 1/starter/script.js
@@ -0,0 +1,24 @@
+let aragorn = 'amazing';
+
+console.log(23);
+console.log(aragorn);
+
+// display calculations
+console.log(40 + 8 + 23 - 10);
+
+// change variable value in different places
+
+let firstName = "Cirilla";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+// variable names
+
+let name_example = "Nukri";
+let $another = 27;
+
+let person = "Nukri";
+let PI = 3.1415;
