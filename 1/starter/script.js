3953e2788c81748f46b65eba3b0ed7f5f7320f48
EarendilM83 <nukri.tusishvili@gmail.com>
2023-08-18 23:52:43 +0400

    strings and template literals

--- 1/starter/script.js
+++ 1/starter/script.js
@@ -1,32 +1,56 @@
-let aragorn = 'amazing';
+// let aragorn = 'amazing';
 
-console.log(23);
-console.log(aragorn);
+// console.log(23);
+// console.log(aragorn);
 
 // display calculations
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 // change variable value in different places
 
-let firstName = "Cirilla";
+// let firstName = "Cirilla";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 
 // variable names
 
-let name_example = "Nukri";
-let $another = 27;
+// let name_example = "Nukri";
+// let $another = 27;
 
-let person = "Nukri";
-let PI = 3.1415;
+// let person = "Nukri";
+// let PI = 3.1415;
 
 // use descriptive names
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+
+const firstName = 'nukri';
+const job = 'teacher';
+const birthYear = '1996';
+const year = 2023;
+
+const Nukri = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+
+console.log(Nukri);
+
+
+const templateLiterals = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+
+console.log(templateLiterals)
+
+
+console.log(`ჩვეულებრივი ტექსტი`);
+
+console.log('New line syntax -> \n\ this is second line already \n\ and so it is');
+
+console.log(`1st line,
+second line,
+3rd line.`)
